a9fe92436b88e98c8ae0050f4879cc0527066bb9
Cujowolf <cujo@bach.local>
2022-08-16 12:15:47 -0500

    Add one sided liquidity changes to volume

--- src/Bean3CRVHandler.ts
+++ src/Bean3CRVHandler.ts
@@ -1,9 +1,8 @@
-import { BigInt, log } from "@graphprotocol/graph-ts";
+import { BigInt, BigDecimal, log } from "@graphprotocol/graph-ts";
 import { AddLiquidity, RemoveLiquidity, RemoveLiquidityImbalance, RemoveLiquidityOne, TokenExchange, TokenExchangeUnderlying } from "../generated/Bean3CRV/Bean3CRV";
 import { CurvePrice } from "../generated/Bean3CRV/CurvePrice";
-import { ZERO_BI } from "./helpers";
 import { CURVE_PRICE } from "./utils/Constants";
-import { ONE_BD, toDecimal } from "./utils/Decimals";
+import { ONE_BD, toDecimal, ZERO_BD, ZERO_BI } from "./utils/Decimals";
 import { loadBean, loadBeanDailySnapshot, loadBeanHourlySnapshot, loadCross } from "./utils/EntityLoaders";
 
 export function handleTokenExchange(event: TokenExchange): void {
@@ -186,22 +185,22 @@ export function handleTokenExchangeUnderlying(event: TokenExchangeUnderlying): v
 }
 
 export function handleAddLiquidity(event: AddLiquidity): void {
-    handleLiquidityChange(event.block.timestamp)
+    handleLiquidityChange(event.block.timestamp, event.params.token_amounts[0], event.params.token_amounts[1])
 }
 
 export function handleRemoveLiquidity(event: RemoveLiquidity): void {
-    handleLiquidityChange(event.block.timestamp)
+    handleLiquidityChange(event.block.timestamp, event.params.token_amounts[0], event.params.token_amounts[1])
 }
 
 export function handleRemoveLiquidityImbalance(event: RemoveLiquidityImbalance): void {
-    handleLiquidityChange(event.block.timestamp)
+    handleLiquidityChange(event.block.timestamp, event.params.token_amounts[0], event.params.token_amounts[1])
 }
 
 export function handleRemoveLiquidityOne(event: RemoveLiquidityOne): void {
-    handleLiquidityChange(event.block.timestamp)
+    handleLiquidityChange(event.block.timestamp, event.params.token_amount, ZERO_BI)
 }
 
-function handleLiquidityChange(timestamp: BigInt): void {
+function handleLiquidityChange(timestamp: BigInt, token0Amount: BigInt, token1Amount: BigInt): void {
     // Get Curve Price Details
     let curvePrice = CurvePrice.bind(CURVE_PRICE)
     let curve = curvePrice.try_getCurve()
@@ -216,18 +215,35 @@ function handleLiquidityChange(timestamp: BigInt): void {
     let newPrice = toDecimal(curve.value.price)
     let deltaLiquidityUSD = toDecimal(curve.value.liquidity).minus(bean.totalLiquidityUSD)
 
+    let volumeUSD = deltaLiquidityUSD < ZERO_BD ? deltaLiquidityUSD.div(BigDecimal.fromString('2')).times(BigDecimal.fromString('-1')) : deltaLiquidityUSD.div(BigDecimal.fromString('2'))
+    let beanVolume = BigInt.fromString(volumeUSD.div(newPrice).times(BigDecimal.fromString('1000000')).truncate(0).toString())
+
+    if (token0Amount !== ZERO_BI && token1Amount !== ZERO_BI) {
+        volumeUSD = ZERO_BD
+        beanVolume = ZERO_BI
+    }
+    bean.totalVolume = bean.totalVolume.plus(beanVolume)
+    bean.totalVolumeUSD = bean.totalVolumeUSD.plus(volumeUSD)
     bean.totalLiquidityUSD = toDecimal(curve.value.liquidity)
     bean.price = toDecimal(curve.value.price)
     bean.save()
 
+    beanHourly.totalVolume = bean.totalVolume
+    beanHourly.totalVolumeUSD = bean.totalVolumeUSD
     beanHourly.totalLiquidityUSD = bean.totalLiquidityUSD
     beanHourly.price = bean.price
     beanHourly.hourlyLiquidityUSD = beanHourly.hourlyLiquidityUSD.plus(deltaLiquidityUSD)
+    beanHourly.hourlyVolume = beanHourly.hourlyVolume.plus(beanVolume)
+    beanHourly.hourlyVolumeUSD = beanHourly.hourlyVolumeUSD.plus(volumeUSD)
     beanHourly.save()
 
+    beanHourly.totalVolume = bean.totalVolume
+    beanHourly.totalVolumeUSD = bean.totalVolumeUSD
     beanDaily.totalLiquidityUSD = bean.totalLiquidityUSD
     beanDaily.price = bean.price
     beanDaily.dailyLiquidityUSD = beanDaily.dailyLiquidityUSD.plus(deltaLiquidityUSD)
+    beanDaily.dailyVolume = beanDaily.dailyVolume.plus(beanVolume)
+    beanDaily.dailyVolumeUSD = beanDaily.dailyVolumeUSD.plus(volumeUSD)
     beanDaily.save()
 
     // Handle a peg cross
